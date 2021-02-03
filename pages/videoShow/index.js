a4de077992bc01dd16bcc0305f10e27df2b3af49
王彩珠 <819137027@qq.com>
2021-02-03 15:13:57 +0800

    支付问题
    支付问题

--- pages/videoShow/index.js
+++ pages/videoShow/index.js
@@ -69,6 +69,9 @@ grace.page({
                   wx.showToast({
                     title: '支付成功',
                   })
+                  let videoData = that.$data.videoData
+                  videoData.template.orderStatus = 1
+                  that.$data.videoData = JSON.parse(JSON.stringify(videoData))
                   clearInterval(interval)
                   that.downLoad()
                 }
