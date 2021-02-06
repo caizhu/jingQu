ea3c51f93ea521930a3bbd278c7e2da33eab2146
jiangpenglong <jiangpenglong@ut.cn>
2021-02-06 15:34:55 +0800

    提交代码

--- pages/bindPhoneNum/index.js
+++ pages/bindPhoneNum/index.js
@@ -7,10 +7,30 @@ const app = getApp()
 grace.page({
   data:{
     wxPhone:'../../images/phone.png',
+    phone:null,
+    code: null
   },
   onLoad(){
     this.$data.user = wx.getStorageSync('user')
   },
+  inputChangeHandler(e){
+    const key = e.currentTarget.dataset.key
+    this.$data[key] = e.detail.value
+  },
+  bindHandler(){
+    if(!this.$data.phone){
+      return wx.showToast({
+        title: '手机号不能为空',
+        icon:'none'
+      })
+    }
+    if(!this.$data.code){
+      return wx.showToast({
+        title: '验证码不能为空',
+        icon:'none'
+      })
+    }
+  }
   
 })
 
